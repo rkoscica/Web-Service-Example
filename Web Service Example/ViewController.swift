df694a06eea8ceb1764c3cc402c99809a2983a67
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-11-24 09:43:58 -0500

    4.2.3 successfully gets data from web and prints in console

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -5,14 +5,25 @@
 //  Created by Rosalyn Koscica on 11/19/15.
 //  Copyright © 2015 Rosalyn Koscica. All rights reserved.
 //
+// API Code:    4e82234da75c8c3c0e87d8f22c5eebac
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forecastLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=4e82234da75c8c3c0e87d8f22c5eebac",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                print("Response: " + responseObject.description)
+            },
+            failure: { (operation:AFHTTPRequestOperation?,error:NSError!) in
+                print("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +33,3 @@ class ViewController: UIViewController {
 
 
 }
-
---
+++ bridge.h
@@ -0,0 +1,24 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Rosalyn Koscica on 11/24/15.
+//  Copyright © 2015 Rosalyn Koscica. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
+
+
+
+
+#import "/Users/rkoscica/Desktop/Code12/Web Service Example/AFNetworking/AFNetworking.h"
+
+
+
+
+
+
